c28e37a3ea1b15def167d67b01d13a5fc7de86a1
duncanmurray <nacnudyarrum@gmail.com>
2013-08-30 18:12:44 -0500

    Working program, needs some cleaning up

--- lsyncd_update.py
+++ lsyncd_update.py
@@ -10,51 +10,41 @@ import os
 from sys import exit
 from pyrax import exceptions as e
 import logging
+from subprocess import call
 
-# Set location of pyrax configuration file
-CREDFILE = "/root/cloudcreds"
+# Set default location of pyrax configuration file
+CREDFILE = "/root/.rackspace_cloud_credentials"
 # Set location of log files
-LOGPATH = "./"
+LOGPATH = "/var/log/"
 # Set default metadata key that defines a server installed with lsyncd
-METAKEY = "who"
+METAKEY = "lsyncd"
 # Set default metadata key of your lsyncd configuration group
-METAVALUE = "duncan"
+METAVALUE = "enabled"
 # Set default location of your lsyncd configuration file
-LSYNCDCONF = "./lsyncd.conf"
+LSYNCDCONF = "/etc/lsyncd.lua"
 # Set default location of lsyncd configuration template
-TEMPLATE = "./template"
+TEMPLATE = "/etc/lsyncd.template"
 
 def main():
-    # Configure log formatting
-    logFormatter = logging.Formatter('%(asctime)s %(levelname)s %(message)s')
-    rootLogger = logging.getLogger()
-    rootLogger.setLevel(logging.DEBUG)
-    # Configure logging to file
-    fileHandler = logging.FileHandler("{0}/{1}.log".format(LOGPATH, os.path.basename(__file__)))
-    fileHandler.setFormatter(logFormatter)
-    rootLogger.addHandler(fileHandler)
-    # Configure loggign to console
-    consoleHandler = logging.StreamHandler()
-    consoleHandler.setFormatter(logFormatter)
-    rootLogger.addHandler(consoleHandler)
 
     # Read in argumants cron command line to over ride defaults
+    # No arguments are required and all can be over riden
     parser = argparse.ArgumentParser(description=("Automatically update lsyncd configuration"))
     parser.add_argument("-r", "--region", action="store", required=False,
                         metavar="region", type=str,
                         help=("Region where your lsyncd configuration group is (defaults"
                               " to 'LON') [ORD, DFW, LON, SYD]") , choices=["ORD", "DFW", "LON", "SYD"],
                         default="LON")
-    parser.add_argument("-k", "--metakey", action="store", required=False,
+    parser.add_argument("-mk", "--metakey", action="store", required=False,
                         metavar="metakey", type=str,
                         help=("Matadata key to search for that identifies lsyncd"
                               " is installed"), 
                         default=METAKEY)
-    parser.add_argument("-v", "--metavalue", action="store", required=False,
+    parser.add_argument("-mv", "--metavalue", action="store", required=False,
                         metavar="metavalue", type=str,
                         help=("Metadata value of your lsyncd configuration group"),
                         default=METAVALUE)
-    parser.add_argument("-c", "--lsyncdconf", action="store", required=False,
+    parser.add_argument("-l", "--lsyncdconf", action="store", required=False,
                         metavar="lsyncdconf", type=str,
                         help=("The location of your lsyncd configuration file"),
                         default=LSYNCDCONF)
@@ -62,10 +52,32 @@ def main():
                         metavar="template", type=str,
                         help=("The location of your lsyncd configuration template"),
                         default=TEMPLATE)
-
+    parser.add_argument("-c", "--credfile", action="store", required=False,
+                        metavar="credfile", type=str,
+                        help=("The location of your pyrax configuration file"),
+                        default=CREDFILE)
+    parser.add_argument("-v", "--verbose", action="store_true", required=False,
+                        help=("Turn on debug verbosity"),
+                        default=False)
 
     # Parse arguments (validate user input)
     args = parser.parse_args()
+
+    # Configure log formatting
+    logFormatter = logging.Formatter('%(asctime)s %(levelname)s %(message)s')
+    rootLogger = logging.getLogger()
+    if args.verbose:
+        rootLogger.setLevel(logging.DEBUG)
+    else:
+        rootLogger.setLevel(logging.WARNING)
+    # Configure logging to file
+    fileHandler = logging.FileHandler("{0}/{1}.log".format(LOGPATH, os.path.basename(__file__)))
+    fileHandler.setFormatter(logFormatter)
+    rootLogger.addHandler(fileHandler)
+    # Configure loggign to console
+    consoleHandler = logging.StreamHandler()
+    consoleHandler.setFormatter(logFormatter)
+    rootLogger.addHandler(consoleHandler)    
 
     # Define the authentication credentials file location and request that
     # pyrax makes use of it. If not found, let the client/user know about it.
@@ -78,7 +90,7 @@ def main():
 
     # Test that the pyrax configuration file provided exists
     try:
-        creds_file = os.path.expanduser(CREDFILE)
+        creds_file = os.path.expanduser(args.credfile)
         pyrax.set_credential_file(creds_file, args.region)
     # Exit if authentication fails
     except e.AuthenticationFailed:
@@ -89,6 +101,13 @@ def main():
     # Exit if file does not exist
     except e.FileNotFound:
         rootLogger.critical("Credentials file '%s' not found" % (creds_file))
+        rootLogger.info("%s", """Use a credential file in the following format:\n
+                                 [rackspace_cloud]
+                                 username = myuseername
+                                 api_key = 01sdf444g3ffgskskeoek0349
+                                 region = LON
+                              """
+                       )
         exit(2)
 
     # Define a function to check if any IP's need updating
@@ -172,18 +191,35 @@ def main():
  
         # Read in the settings block from configuration templatea
         with open(args.template) as template_file:
-            # Set start of block
             for line in template_file:
+                # Set start of block
                 if line.strip() == 'SETTINGS_START':
                     break
-            # Set end of block
             for line in template_file:
+                # Set end of block
                 if line.strip() == 'SETTINGS_END':
                     break
                 # Write lines to file
                 lfile.write("%s" % line)
+        
+        # Read in the sync block(s) from the configuration template
+        with open(args.template) as template_file:
+            for ip in active_ips:
+                for line in template_file:
+                    # Set start block
+                    if line.strip() == 'SYNC_START':
+                        break
+                for line in template_file:
+                    # Set end block
+                    if line.strip() == 'SYNC_END':
+                        break
+                    # Replace IPREPLACE with active ip's
+                    lfile.write("%s" % line.replace('IPREPLACE', ip))
+                lfile.close()
 
-        print "need to write server block"        
+        # Restart lsyncd process
+        rootLogger.warning("Restarting lsyncd service")
+        call(["/usr/sbin/service", "cron", "restart"])
 
 if __name__ == '__main__':
     main()
